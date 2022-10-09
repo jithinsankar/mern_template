52146daedaee911cc264bd17128f980519208b76
jithinsankar <jithinsankar.nk@gmail.com>
2022-10-09 23:33:56 +0530

    dashboard index

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -5,6 +5,7 @@ const Dashboard = () => {
 
     const navigate = useNavigate();
     const [quote,setQuote] = useState('')
+    const [tempQuote,setTempQuote]=useState('')
 
     async function populateQuote(){
         const req = await fetch('http://localhost:1337/api/quote',{
@@ -13,7 +14,7 @@ const Dashboard = () => {
             }
         })
 
-        const data = req.json()
+        const data = await req.json()
         console.log(data)
         if(data.status==='ok'){
             setQuote(data.quote)
@@ -36,7 +37,38 @@ const Dashboard = () => {
         }
     },[navigate])
 
-    return <h1>Your quote:{quote||'No quote found'}Hello World</h1>
+    async function updateQuote(event){
+        event.preventDefault()
+        const req = await fetch('http://localhost:1337/api/quote',{
+            method:'POST',
+            headers:{
+                'Content-Type': 'application/json',
+                'x-access-token':localStorage.getItem('token'),
+            },
+            body:JSON.stringify({
+                quote:tempQuote,
+            }),
+        })
+
+        const data = await req.json()
+        console.log(data)
+        if(data.status==='ok'){
+            setQuote(tempQuote)
+            setTempQuote('')
+        }else{
+            alert(data.error)
+        }
+    }
+
+    return (<div>
+                <h1>Your quote:{quote||'No quote found'}Hello World</h1>\
+                <form onSubmit={updateQuote}>
+                    <input type="text" placeholder="Quote" value={tempQuote} onChange={e=>setTempQuote(e.target.value)}/>
+                    <input type="submit" value="Update Quote"/>
+                </form>
+
+            </div>
+)
 }
 
 export default Dashboard
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const User = require('./models/user.model')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
 app.use(cors())
 app.use(express.json())
@@ -14,10 +15,11 @@ mongoose.connect('mongodb://localhost:27017/merncd')
 app.post('/api/register',async (req,res)=>{
     console.log(req.body)
     try{
+        const newPassword = await bcrypt.hash(req.body.password, 10)
         await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: newPassword,
         })
         res.json({status:'ok'})
     }catch(err){
@@ -28,10 +30,16 @@ app.post('/api/login',async(req,res)=>{
 
     const user = await User.findOne({
         email:req.body.email,
-        password:req.body.password,
-    })
  
-    if(user){
+    })
+    
+    if(!user){
+        return {status:'error',error:'Invalid login'}
+    }
+
+    const isPasswordValid = await bcrypt.compare(req.body.password, user.password)
+
+    if(isPasswordValid){
         const  token = jwt.sign({
             name:user.name,
             email:user.email,
@@ -53,7 +61,7 @@ app.get('/api/quote',async(req,res)=>{
         const email =decoded.email
         const user = await User.findOne({email:email})
 
-        return {status:'ok',quote:user.quote}
+        return res.json({status:'ok',quote:user.quote})
     } catch(error){
         console.log(error)
         res.json({status:'error',error:'invalid token'})
@@ -69,9 +77,9 @@ app.post('/api/quote',async(req,res)=>{
     try{
         const decoded = jwt.verify(token,'secret123')
         const email =decoded.email
-        await User.updateOne({email:email},{$set:{quote:req.body.quote}})
+        const user = await User.updateOne({email:email},{$set:{quote:req.body.quote}})
 
-        return {status:'ok',quote:user.quote}
+        return res.json({status:'ok',quote:user.quote})
     } catch(error){
         console.log(error)
         res.json({status:'error',error:'invalid token'})
